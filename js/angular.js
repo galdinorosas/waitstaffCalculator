cac246c2665c236ae173d9fde85403e60dcf081e
galdinorosas <dino.rosas@gmail.com>
2016-03-02 23:10:14 -0800

    angular routes are added in. This involved using $routeProvider service. However my values seem to be resetting on every page change.

--- js/angular.js
+++ js/angular.js
@@ -1,61 +1,83 @@
-angular.module('myApp',[])
-.controller('myCtrl', function($scope){
+angular.module('myApp',['ngRoute'])
+	.config(['$routeProvider', function($routeProvider){
+		$routeProvider.when('/',{
+			templateUrl:'home.html',
+			controller: 'homeCtrl'
+		})
+		.when('/new-meal',{
+			templateUrl: 'newMeal.html',
+			controller:'myCtrl'
 
-	$scope.mealPrice = 0;
-	$scope.taxRate = 0;
-	$scope.tipPercentage = 0;
-	$scope.subtotal = 0;
-	$scope.tip = 0;
-	$scope.total = 0;
-	$scope.tipTotal = 0;
-	$scope.mealCount = 0;
-	$scope.avgTipPerMeal = 0;
+		})
+		.when('/my-earnings',{
+			templateUrl: 'myEarnings.html',
+			controller: 'myCtrl'
 
-	$scope.submit= function(){
-		console.log($scope.mealForm);
-		if($scope.mealForm.$valid ) {
+		});
 
-		   	$scope.subtotal = 0;
+	}])
+		.controller('myCtrl',[ '$scope',function($scope){
+
+			$scope.mealPrice = 0;
+			$scope.taxRate = 0;
+			$scope.tipPercentage = 0;
+			$scope.subtotal = 0;
 			$scope.tip = 0;
 			$scope.total = 0;
-			$scope.subtotal = $scope.mealPrice * (1 + $scope.taxRate/100);
-			$scope.tip = $scope.mealPrice * ($scope.tipPercentage/100);
-			$scope.total = $scope.subtotal + $scope.tip;
+			$scope.tipTotal = 0;
+			$scope.mealCount = 0;
+			$scope.avgTipPerMeal = 0;
 
-			$scope.tipTotal = $scope.tipTotal + $scope.tip;
-			$scope.mealCount = $scope.mealCount + 1;
-			$scope.avgTipPerMeal = $scope.tipTotal / $scope.mealCount;
+			$scope.submit= function(){
+				console.log($scope.mealForm);
+				if($scope.mealForm.$valid ) {
 
-		} 
-		else {
-		    console.log('The form is invalid');
-		}
-		
+				   	$scope.subtotal = 0;
+					$scope.tip = 0;
+					$scope.total = 0;
+					$scope.subtotal = $scope.mealPrice * (1 + $scope.taxRate/100);
+					$scope.tip = $scope.mealPrice * ($scope.tipPercentage/100);
+					$scope.total = $scope.subtotal + $scope.tip;
 
-	};
+					$scope.tipTotal = $scope.tipTotal + $scope.tip;
+					$scope.mealCount = $scope.mealCount + 1;
+					$scope.avgTipPerMeal = $scope.tipTotal / $scope.mealCount;
 
-	$scope.cancel= function(){
-		$scope.mealPrice = 0;
-		$scope.taxRate = 0;
-		$scope.tipPercentage = 0;
-	};
+				} 
+				else {
+				    console.log('The form is invalid');
+				}
+				
 
-	$scope.reset= function(){
-		$scope.mealPrice = 0;
-		$scope.taxRate = 0;
-		$scope.tipPercentage = 0;
-		$scope.subtotal = 0;
-		$scope.tip = 0;
-		$scope.total = 0;
-		$scope.tipTotal = 0;
-		$scope.mealCount = 0;
-		$scope.avgTipPerMeal = 0;
-	};
+			};
+
+			$scope.cancel= function(){
+				$scope.mealPrice = 0;
+				$scope.taxRate = 0;
+				$scope.tipPercentage = 0;
+			};
+
+			$scope.reset= function(){
+				$scope.mealPrice = 0;
+				$scope.taxRate = 0;
+				$scope.tipPercentage = 0;
+				$scope.subtotal = 0;
+				$scope.tip = 0;
+				$scope.total = 0;
+				$scope.tipTotal = 0;
+				$scope.mealCount = 0;
+				$scope.avgTipPerMeal = 0;
+			};
+
+		}])
+		.controller('homeCtrl', ['$scope', function($scope){
 
 
 
 
-
-
-
-});
+		}])
+		.run(function($rootScope, $location) {
+		    $rootScope.$on('$routeChangeError', function() {
+		        $location.path('/');
+		    });
+		});;
